b8f1e0101553d5253207af5ce31c525a4e379c70
Maxim Sofronov <imaxsof@gmail.com>
2018-12-28 23:15:07 +0300

    no message

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,37 @@
-const webpack = require('webpack');
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const config = {
-  entry: './src/index.js',
+module.exports = {
+  entry: {
+    app: './src/components/index.js',
+  },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'js/[name].[hash].js',
+    path: path.resolve(__dirname, 'build')
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
         loader: 'vue-loader'
-      },
-      {
+      }, {
         test: /\.js$/,
         loader: 'babel-loader'
       }
     ]
   },
-  resolve: {
-    extensions: [
-      '.js',
-      '.vue'
-    ]
-  },
   plugins: [
-    new VueLoaderPlugin()
+    new CleanWebpackPlugin(['build']),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/html/index.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: '404.html',
+      template: 'src/html/404.html'
+    }),
   ]
 }
-
-module.exports = config;
